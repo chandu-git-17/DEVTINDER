a4483b7a16ad15c40938087b1a09334cd562af73
chandu-git-17 <chandrashekar.yamasani@gmail.com>
2024-12-21 22:31:05 +0530

    SIGNUP API

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
+app.use("/", express.json());
 
-app.use("/admin",(req, res) => {
-    res.end("Hello...")
+app.post("/signup", async (req, res) => {
+    const user = new User(req.body);
+
+    await user.save();
+    res.send("User added successfully...");
+
+});
+
+
+
+
+
+
+
+connectDB()
+.then(() => {
+    console.log("database connected sucessfully...");
+    app.listen(3000, () => {
+        console.log("Server is listening...")
+    });
 })
-
-
-app.listen(3000, () => {
-    console.log("Server is listening...")
+.catch((error) => {
+    console.log("something went wrong while connecting to database...");
 });
